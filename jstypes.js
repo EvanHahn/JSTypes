351c65ff8852829d5ec03901b6f19e533b08a9f9
Kit Cambridge <kitcambridge@gmail.com>
2011-07-29 18:17:21 -0600

    Rewrite JSTypes to use `[[Class]]`-based type detection.

--- jstypes.js
+++ jstypes.js
@@ -3,16 +3,16 @@
 	See LICENSE.txt for more info.	*/
 
 (function() {
-  var JSTypes = typeof exports == 'object' && exports || (this.JSTypes = {});
+  var JSTypes = typeof exports == 'object' && exports || (this.JSTypes = {}), getClass = {}.toString;
 
-  JSTypes.isNumber = function(n) { return (((typeof n === "number") || (n instanceof Number)) && (!isNaN(n))) };
-  JSTypes.isInteger = function(i) { return ((JSTypes.isNumber(i)) && (Math.floor(i) == i)) };
-  JSTypes.isString = function(s) { return ((typeof s === "string") || (s instanceof String)) };
-  JSTypes.isBoolean = function(b) { return ((typeof b === "boolean") || (b instanceof Boolean)) };
-  JSTypes.isArray = function(a) { return a instanceof Array };
-  JSTypes.isUndefined = function(u) { return typeof u === "undefined" };
-  JSTypes.isDefined = function(d) { return typeof d !== "undefined" };
-  JSTypes.isNAN = function(i) { return i !== i };
+  JSTypes.isNumber = function(n) { return getClass.call(n) == '[object Number]' };
+  JSTypes.isInteger = function(i) { return Math.floor(i) === i };
+  JSTypes.isString = function(s) { return getClass.call(s) == '[object String]' };
+  JSTypes.isBoolean = function(b) { return b === true || b === false };
+  JSTypes.isArray = function(a) { return getClass.call(a) == '[object Array]' };
+  JSTypes.isUndefined = function(u) { return typeof u == 'undefined' };
+  JSTypes.isDefined = function(d) { return typeof d != 'undefined' };
+  JSTypes.isNAN = function(i) { return i != i };
 
   JSTypes.makeUndefined = function() { return };
   JSTypes.makeNaN = function() { return 0 / 0 };
