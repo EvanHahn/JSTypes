ae7ac2a32ee263c499891ab2e581cfb28e74d352
Kit Cambridge <kitcambridge@gmail.com>
2011-08-01 20:50:10 -0600

    Fix the `isInteger` and `isBoolean` methods.

--- jstypes.js
+++ jstypes.js
@@ -6,9 +6,9 @@
   var JSTypes = typeof exports == 'object' && exports || (this.JSTypes = {}), getClass = {}.toString;
 
   JSTypes.isNumber = function(n) { return getClass.call(n) == '[object Number]' };
-  JSTypes.isInteger = function(i) { return Math.floor(i) === i };
+  JSTypes.isInteger = function(i) { return JSTypes.isNumber(i) && Math.floor(i) == i };
   JSTypes.isString = function(s) { return getClass.call(s) == '[object String]' };
-  JSTypes.isBoolean = function(b) { return b === true || b === false };
+  JSTypes.isBoolean = function(b) { return getClass.call(b) == '[object Boolean]' };
   JSTypes.isArray = function(a) { return getClass.call(a) == '[object Array]' };
   JSTypes.isUndefined = function(u) { return typeof u == 'undefined' };
   JSTypes.isDefined = function(d) { return typeof d != 'undefined' };
